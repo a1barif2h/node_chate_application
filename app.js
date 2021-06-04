77adf97adff979367a0386a99a46757fccccd3da
a1barif2h <mohammadarifhossain80@gmail.com>
2021-06-05 00:52:57 +0600

    initial setup is done and database connection is done

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+dotenv.config();
+
+//database connection
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('database connection successful'))
+.catch(err => console.log(err));
+
+//request parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname,"public")));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECURITY))
+
+//routing setup
+
+//error handling
+
+app.listen(process.env.PORT, () => {
+    console.log(`app listening to port ${process.env.PORT}`)
+})
